d47659718974ef6b585f64411a0287e1cf2a8447
Juan Boza <Juanboza80@gmail.com>
2021-03-16 23:46:57 -0600

    broke

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const apiRoutes = require("./apiRoutes");
 const htmlRoutes = require("./htmlRoutes");
 const fs = require("fs");
+const uuidv1 = require("uuidv1");
 
 const app = express();
 const saveFile = "./db/db.json";
@@ -24,28 +25,12 @@ let noteList = fs.existsSync(saveFile)
 
 
 
-app.get("/api/notes", (req, res) => {
-  res.send(noteList);
-});
 
-app.delete("/api/notes/:id", (req, res) => {
-  let deleteNote = req.params.id;
-  noteList = noteList.filter((note) => note.id != deleteNote);
-  fs.writeFileSync(saveFile, JSON.stringify(noteList));
-  res.end("Note has been deleted");
-});
-
-app.post("/api/notes", function (req, res) {
-  let noteData = {
-    title: req.body.title,
-    text: req.body.text,
-    id: uuidv1(),
-  };
   noteList.push(noteData);
   console.log(noteData);
   fs.writeFileSync(saveFile, JSON.stringify(noteList));
   res.end();
-});
+
 
 app.listen(PORT, () => {
   console.log(`Listening ${PORT}`);
