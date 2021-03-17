3dedb1f178d866ed254e09bb60f9aa317b3efb4c
Juan Boza <Juanboza80@gmail.com>
2021-03-17 00:13:25 -0600

    broken?

--- apiRoutes.js
+++ apiRoutes.js
@@ -1,5 +1,6 @@
+/*
 const router = require("express").Router();
-
+const store = require("..db/store");
 
 
 router.get("/notes", (req, res) => {
@@ -25,3 +26,4 @@ router.delete("/notes/:id", (req, res) => {
 });
 
 module.exports = router;
+*/ 
